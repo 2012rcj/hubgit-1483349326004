327fcc12a41ccd0b3409ced04b9b4000352ad1a3
99rstreet <2012rcj@gmail.com>
2017-03-17 18:43:25 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -144,8 +144,31 @@ while ($row = db2_fetch_assoc($stmt)) {
 									</div>
 									<div class="panel-body">
 
+<?php
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/png"))
 
-<img src="images/
+&& ($_FILES["file"]["size"] < 100000000))
+{
+if ($_FILES["file"]["error"] > 0)
+{
+echo "File Error : " . $_FILES["file"]["error"] . "<br />";
+}else {
+ $_FILES["file"]["name"] . "<br />";
+ $_FILES["file"]["type"] . "<br />";
+ ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
+
+
+if (file_exists("images/".$_FILES["file"]["name"]))
+{
+echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
+}else
+{
+move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
+"Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
+?>
+										<img src="images/
 <?php
 $_SESSION['file']=$_FILES["file"]["name"]; 
 echo $_SESSION['file']; 
