c92f5474e02d56003ccba3fb4399ca86def28e27
99rstreet <2012rcj@gmail.com>
2017-03-17 15:26:51 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-     
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -144,30 +144,6 @@ while ($row = db2_fetch_assoc($stmt)) {
 									</div>
 									<div class="panel-body">
 
-<?php
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/png"))
-
-&& ($_FILES["file"]["size"] < 100000000))
-{
-if ($_FILES["file"]["error"] > 0)
-{
-echo "File Error : " . $_FILES["file"]["error"] . "<br />";
-}else {
- $_FILES["file"]["name"] . "<br />";
- $_FILES["file"]["type"] . "<br />";
- ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
-
-
-if (file_exists("images/".$_FILES["file"]["name"]))
-{
-echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
-}else
-{
-move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
-"Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
-?>
 
 <img src="images/
 <?php
