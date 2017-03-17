adf05f0c9034fb6166e68efd304b877db7aedecb
99rstreet <2012rcj@gmail.com>
2017-03-17 12:52:56 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -169,7 +169,7 @@ move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"
 "Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
 ?>
 
-<img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" style="width:100%;height:50%"  >
+<img src="<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" style="width:100%;height:50%"  >
 <?php
 }
 }
