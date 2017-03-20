2f0ac53bb0b2bf445dcc414addc687f5d74248a7
99rstreet <2012rcj@gmail.com>
2017-03-20 13:18:12 +0530

    Update search.php

--- search.php
+++ search.php
@@ -16,7 +16,7 @@ Search: <input type="text" name="term" /><br />
 include 'db_const.php';
 if (!empty($_REQUEST['term'])) {
 
-$term = mysql_real_escape_string($_REQUEST['term']);     
+$term = $_REQUEST['term'];     
 
 
 $stmt = "SELECT * FROM RETAIL_STORE_PROD WHERE STORE_ID='1462908501',Prod_Nm LIKE '%".$term."%'";
@@ -32,7 +32,7 @@ $stmt = "SELECT * FROM RETAIL_STORE_PROD WHERE STORE_ID='1462908501',Prod_Nm LIK
             		
                     Price:<big style="color:green">
                     	Rs<?php echo $row['MRP']?></big><br /><br />
-                                   
+                                  
                
  <a class="btnLink" href="viewCart.php?action=addToCart&PROD_ID=<?php echo $row["PROD_ID"]; ?>" onClick="return popup(this, 'notes')">Add to cart</a></in>
                   </td></td>
