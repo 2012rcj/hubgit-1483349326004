9c47f737ef98edace964a5167f1866c162591017
99rstreet <2012rcj@gmail.com>
2017-03-22 16:35:14 +0530

    Update search1.php

--- search1.php
+++ search1.php
@@ -2,53 +2,57 @@
 <head>
 	
 	<script type='text/javascript' src='js/jquery-1.4.2.min.js'></script>
+<script>
 
+$('#menu-button').click(function(){
+     $('#hide-menu').toggleClass('show-menu');
+});
+</script>
 <style>
-body
-    {
-       overflow:hidden;
-    }
+#s-results {
+    position: fixed;
 
-.menu-button
-    {
-        border:1px solid #000;
-        color:fff;
-        display:inline-block;
-        cursor:pointer;
-    }
+    top: 0px;
+    height: 10px;
+    left: 0px;
+    right: 0px;
 
-.hide-menu
-{
-    background-color:#336ca6;
-    position: absolute;
-    top:0;
-    right:0;
-    z-index: 1;
-    width: 300px;
-    height: 100%;
-    -webkit-transform: translate3d(300px,0,0);
-    -moz-transform: translate3d(300px,0,0);
-    -o-transform: translate3d(300px,0,0);
-    -ms-transform: translate3d(300px,0,0);
-    transform: translate3d(300px,0,0);      
-    -webkit-transition: all 0.3s linear;
-    -moz-transition: all 0.3s linear;
-    -ms-transition: all 0.3s linear;
-    -o-transition: all 0.3s linear;
-    transition: all 0.3s linear;
+    background-color: yellow;
+    -webkit-transition-duration: 0.3s;
+    -moz-transition-duration: 0.3s;
+    -o-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-duration: 0.3s;
 }
 
-.show-menu      
-{
-    -webkit-transform: translate3d(0,0,0);
-    -moz-transform: translate3d(0,0,0);
-    -o-transform: translate3d(0,0,0);
-    -ms-transform: translate3d(0,0,0);
-    transform: translate3d(0,0,0);
- }  
+#s-results:hover {
+    top: 50px;
+    background-color: cyan;
+}
+
+#s-results_inner {
+    position: fixed;
+
+    top: -50px;
+    height: 50px;
+    left: 0px;
+    right: 0px;
+
+    background-color: red;
+    -webkit-transition-duration: 0.3s;
+    -moz-transition-duration: 0.3s;
+    -o-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+}
+
+#s-results:hover #s-results_inner {
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    background-color: magenta;
+} 
 </style>
 </head>
-
 <body>
 <center>
 <br>
@@ -64,16 +68,10 @@ body
 <br>
 
 <div id = "s-results">
-<nav id="hide-menu" class="hide-menu">
 	<!-- Search results here! -->
-</nav>
 </div>
 
 <script type = "text/javascript">
-
-$('#menu-button').click(function(){
-     $('#hide-menu').toggleClass('show-menu');
-});
 $(document).ready(function(){
 	$('#s-results').load('search.php').show();
 	
@@ -103,6 +101,6 @@ $(document).ready(function(){
 </script>
 
 </center>
-
+</nav>
 </body>
 </html>
