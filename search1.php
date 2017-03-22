ce5a9ceac16bb948176f8c932a9c20b7e2399156
99rstreet <2012rcj@gmail.com>
2017-03-22 12:09:58 +0530

    Update search1.php

--- search1.php
+++ search1.php
@@ -2,7 +2,56 @@
 <head>
 	
 	<script type='text/javascript' src='js/jquery-1.4.2.min.js'></script>
+<script>
 
+$('#menu-button').click(function(){
+     $('#hide-menu').toggleClass('show-menu');
+});
+</script>
+<style>
+body
+    {
+       overflow:hidden;
+    }
+
+.menu-button
+    {
+        border:1px solid #000;
+        color:fff;
+        display:inline-block;
+        cursor:pointer;
+    }
+
+.hide-menu
+{
+    background-color:#336ca6;
+    position: absolute;
+    top:0;
+    right:0;
+    z-index: 1;
+    width: 300px;
+    height: 100%;
+    -webkit-transform: translate3d(300px,0,0);
+    -moz-transform: translate3d(300px,0,0);
+    -o-transform: translate3d(300px,0,0);
+    -ms-transform: translate3d(300px,0,0);
+    transform: translate3d(300px,0,0);      
+    -webkit-transition: all 0.3s linear;
+    -moz-transition: all 0.3s linear;
+    -ms-transition: all 0.3s linear;
+    -o-transition: all 0.3s linear;
+    transition: all 0.3s linear;
+}
+
+.show-menu      
+{
+    -webkit-transform: translate3d(0,0,0);
+    -moz-transform: translate3d(0,0,0);
+    -o-transform: translate3d(0,0,0);
+    -ms-transform: translate3d(0,0,0);
+    transform: translate3d(0,0,0);
+ }  
+</style>
 </head>
 <body>
 <center>
@@ -13,8 +62,8 @@
 </div>
 
 <form action="" name = "form">	
-	<input type="text" name="name" id="fn" Placeholder="Search Something..." style="width:100px; padding:5px;"/>
-	<input type="submit" value="Search"  style="padding:5px;"/>
+	<input type="text" name="name" id="fn" Placeholder="Search Something..." style="width:300px; padding:8px;"/>
+	<input type="submit" value="Search" id="menu-button"style="padding:8px;"/>
 </form>
 <br>
 
