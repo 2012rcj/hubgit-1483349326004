845f44f01ac8aaab5983a15af74ae4ff141d8d5f
99rstreet <2012rcj@gmail.com>
2017-03-28 15:55:50 +0530

    Update file.php

--- file.php
+++ file.php
@@ -149,10 +149,81 @@ while ($row = db2_fetch_assoc($stmt)) {
 									<h3 class="panel-title"><span class="fa fa-pencil-square-o"></span> Upload Your Priscription </h3>
 									</div>
 									<div class="panel-body">
-			<form action="billing.php" method="POST" enctype="multipart/form-data">
-	            	<input type="file" name="file" /><br>
-	            	<input type="submit" class="btn" value="Upload">
-	          	</form>
+			 <script>
+
+var abc = 0; //Declaring and defining global increement variable
+
+$(document).ready(function() {
+
+//To add new input file field dynamically, on click of "Add More Files" button below function will be executed
+    $('#add_more').click(function() {
+        $(this).before($("<div/>", {id: 'filediv'}).fadeIn('slow').append(
+                $("<input/>", {name: 'file[]', type: 'file', id: 'file'}),        
+                $("<br/><br/>")
+                ));
+    });
+
+//following function will executes on change event of file input to select different file	
+$('body').on('change', '#file', function(){
+            if (this.files && this.files[0]) {
+                 abc += 1; //increementing global variable by 1
+				
+				var z = abc - 1;
+                var x = $(this).parent().find('#previewimg' + z).remove();
+                $(this).before("<div id='abcd"+ abc +"' class='abcd'><img id='previewimg" + abc + "' src=''/></div>");
+               
+			    var reader = new FileReader();
+                reader.onload = imageIsLoaded;
+                reader.readAsDataURL(this.files[0]);
+               
+			    $(this).hide();
+                $("#abcd"+ abc).append($("<img/>", {id: 'img', src: 'x.png', alt: 'delete'}).click(function() {
+                $(this).parent().parent().remove();
+                }));
+            }
+        });
+
+//To preview image     
+    function imageIsLoaded(e) {
+        $('#previewimg' + abc).attr('src', e.target.result);
+    };
+
+    $('#upload').click(function(e) {
+        var name = $(":file").val();
+        if (!name)
+        {
+            alert("First Image Must Be Selected");
+            e.preventDefault();
+        }
+    });
+});
+
+</script>
+		
+		<!-------Including CSS File------>
+        
+           <div id="maindiv">
+
+            <div id="formdiv">
+        
+                <form enctype="multipart/form-data" action="billing.php" method="post">
+                    
+            
+                    <div id="filediv"><input name="file[]" type="file" id="file"/></div><br/>
+           
+                    <input type="button" id="add_more" class="btn" value="Add More Files"/>
+                    <input type="submit" value="Upload File" name="submit" id="upload" class="btn"/>
+                </form>
+
+                <br/>
+                <br/>
+				
+              
+            </div>
+           
+		   
+            </div>
+        </div>
 
 
 							</div>				
