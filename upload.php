876e5e1a7af7532bf69e49a1a27a4a6280d3f56e
99rstreet <2012rcj@gmail.com>
2017-03-28 15:06:48 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,6 +7,7 @@ if (isset($_POST['submit'])) {
 
         $validextensions = array("jpeg", "jpg", "png");  //Extensions which are allowed
         $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
+$File_Name= basename($_FILES["file"]["name"][$i]);
         $file_extension = end($ext); //store extensions in the variable
         
 		$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
@@ -16,7 +17,7 @@ if (isset($_POST['submit'])) {
                 && in_array($file_extension, $validextensions)) {
             if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
                 echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
-                              echo '<img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" height="65%" width="65%">'; 
+                            echo $File_Name. "</br>";
             } else {//if file was not moved.
                 echo $j. ').<span id="error">please try again!.</span><br/><br/>';
             }
