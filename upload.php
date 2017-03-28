2a20aa0d85cebb4b3fdf5129912844763a98a4f9
99rstreet <2012rcj@gmail.com>
2017-03-28 18:03:06 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,12 +2,11 @@
 if (isset($_POST['submit'])) {
     $j = 0; //Variable for indexing uploaded image 
     
-	$target_path = "images/"; //Declaring Path for uploaded images
+	$target_path = "image/"; //Declaring Path for uploaded images
     for ($i = 0; $i < count($_FILES['file']['name']); $i++) {//loop to get individual element from the array
 
         $validextensions = array("jpeg", "jpg", "png");  //Extensions which are allowed
-        $ext = explode('.', basename("images/". $_FILES['file']['name'][$i]));//explode file name from dot(.) 
-$File_Name= images/$_FILES["file"]["name"];
+        $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
         $file_extension = end($ext); //store extensions in the variable
         
 		$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
@@ -17,11 +16,6 @@ $File_Name= images/$_FILES["file"]["name"];
                 && in_array($file_extension, $validextensions)) {
             if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
                 echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
-                            echo $File_Name. "</br>";
-                          $var_holding_img = "<img src='$File_Name' alt='picture' width='150' height='150'><br/></img>"; 
-                              $string =   $var_holding_img ;
-$_SESSION['File_Name'] = $string;
-echo $_SESSION['File_Name']; 
             } else {//if file was not moved.
                 echo $j. ').<span id="error">please try again!.</span><br/><br/>';
             }
@@ -31,4 +25,3 @@ echo $_SESSION['File_Name'];
     }
 }
 ?>
-
