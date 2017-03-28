2a2b9d36070e0eb20d7f463360c0ae08fde61e80
99rstreet <2012rcj@gmail.com>
2017-03-28 14:43:09 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,34 +1,27 @@
 <?php
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/png"))
+if (isset($_POST['submit'])) {
+    $j = 0; //Variable for indexing uploaded image 
+    
+	$target_path = "uploads/"; //Declaring Path for uploaded images
+    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {//loop to get individual element from the array
 
-&& ($_FILES["file"]["size"] < 100000000))
-{
-if ($_FILES["file"]["error"] > 0)
-{
-echo "File Error : " . $_FILES["file"]["error"] . "<br />";
-}else {
- $_FILES["file"]["name"] . "<br />";
- $_FILES["file"]["type"] . "<br />";
- ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
-
-
-if (file_exists("images/".$_FILES["file"]["name"]))
-{
-echo "<b>".$_FILES["file"]["name"] . " already exists. </b>";
-}else
-{
-move_uploaded_file($_FILES["file"]["tmp_name"],"images/". $_FILES["file"]["name"]);
-"Stored in: " . "images/" . $_FILES["file"]["name"]."<br />";
-?>
-
-<img src="images/<?php echo $_FILES["file"]["name"]; ?>" alt="Image path Invalid" >
-<?php
-}
-}
-}else
-{
-echo "Invalid file detail ::<br> file type ::".$_FILES["file"]["type"]." , file size::: ".$_FILES["file"]["size"];
+        $validextensions = array("jpeg", "jpg", "png");  //Extensions which are allowed
+        $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
+        $file_extension = end($ext); //store extensions in the variable
+        
+		$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
+        $j = $j + 1;//increment the number of uploaded images according to the files in array       
+      
+	  if (($_FILES["file"]["size"][$i] < 100000) //Approx. 100kb files can be uploaded.
+                && in_array($file_extension, $validextensions)) {
+            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
+                echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
+            } else {//if file was not moved.
+                echo $j. ').<span id="error">please try again!.</span><br/><br/>';
+            }
+        } else {//if file size and file type was incorrect.
+            echo $j. ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
+        }
+    }
 }
 ?>
