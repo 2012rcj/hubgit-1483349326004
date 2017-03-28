b24e433864601d604672baf70137cc0231bdb3e1
99rstreet <2012rcj@gmail.com>
2017-03-28 15:03:52 +0530

    Update search1.php

--- search1.php
+++ search1.php
@@ -1,89 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-	    <style>
-		    
-		    @import url(http://fonts.googleapis.com/css?family=Droid+Sans);
-form{
-    background-color:white;
-}
-#maindiv{
-    width:960px; 
-    margin:10px auto; 
-    padding:10px;
-    font-family: 'Droid Sans', sans-serif;
-}
-#formdiv{
-    width:500px; 
-    float:left; 
-    text-align: center;
-}
-form{
-    padding: 40px 20px;
-    box-shadow: 0px 0px 10px;
-    border-radius: 2px;
-}
-h2{
-    margin-left: 30px;
-}
-.upload{
-    background-color:#ff0000;
-    border:1px solid #ff0000;
-    color:#fff;
-    border-radius:5px;
-    padding:10px;
-    text-shadow:1px 1px 0px green;
-    box-shadow: 2px 2px 15px rgba(0,0,0, .75);
-}
-.upload:hover{
-    cursor:pointer;
-    background:#c20b0b;
-    border:1px solid #c20b0b;
-    box-shadow: 0px 0px 5px rgba(0,0,0, .75);
-}
-#file{
-    color:green;
-    padding:5px; border:1px dashed #123456;
-    background-color: #f9ffe5;
-}
-#upload{
-    margin-left: 45px;
-}
-
-#noerror{
-    color:green;
-    text-align: left;
-}
-#error{
-    color:red;
-    text-align: left;
-}
-#img{ 
-    width: 17px;
-    border: none; 
-    height:17px;
-    margin-left: -20px;
-    margin-bottom: 91px;
-}
-
-.abcd{
-    text-align: center;
-}
-
-.abcd img{
-    height:100px;
-    width:100px;
-    padding: 5px;
-    border: 1px solid rgb(232, 222, 189);
-}
-b{
-    color:red;
-}
-#formget{
-    float:right; 
-
-}
-		    </style>
+		  
 		<title>Upload Multiple Images Using jquery and PHP</title>
 		<!-------Including jQuery from google------>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -144,7 +62,7 @@ $('body').on('change', '#file', function(){
             <div id="formdiv">
                 <h2>Multiple Image Upload Form</h2>
                 <form enctype="multipart/form-data" action="" method="post">
-                    First Field is Compulsory. Only JPEG,PNG,JPG Type Image Uploaded. Image Size Should Be Less Than 100KB.
+                   
                     <hr/>
                     <div id="filediv"><input name="file[]" type="file" id="file"/></div><br/>
            
