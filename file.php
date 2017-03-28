0a58fa0a67be0d154bab863401ba084d6ce83f07
99rstreet <2012rcj@gmail.com>
2017-03-28 16:25:09 +0530

    Update file.php

--- file.php
+++ file.php
@@ -150,10 +150,8 @@ while ($row = db2_fetch_assoc($stmt)) {
 									</div>
 									<div class="panel-body">
 	<form action="billing.php" method="POST" enctype="multipart/form-data">
-	            	<input type="file" name="file[]" /><br>
-		 <input name="file[]" type="file" /><br />
-  <input name="file[]" type="file" /><br />
-		 <input name="file[]" type="file" /><br />
+	            	<input type="file" name="file" /><br>
+		
 	            	<input type="submit" class="btn" value="Upload">
 	          	</form>
 
