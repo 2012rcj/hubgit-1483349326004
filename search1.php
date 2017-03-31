32e1356bf48de52918ff713379314f0738940ea5
99rstreet <2012rcj@gmail.com>
2017-03-31 12:18:33 +0530

    Update search1.php

--- search1.php
+++ search1.php
@@ -1,34 +1,42 @@
 <?php
-
-include 'db_const.php';
-
-
-
-        //get rows query
-        $stmt = "SELECT * FROM retail_priscription WHERE priscription='1490872071'";
-       $result = db2_prepare($conn, $stmt);
-       db2_execute($result);
-           
-        if(!empty($stmt)){
-        
-            while($row = db2_fetch_array($result)){
-          $temp=$row['2'];
-echo $temp;
-foreach($temp as $image)
-{
-$ima="images/".$image;
-
-
- echo "<img src='$ima' height='100' width='200' />";
-
+/*  
+Parameter Example
+	$data = array('post_id'=>'12345','post_title'=>'A Blog post');
+	$target = 'single tocken id or topic name';
+	or
+	$target = array('token1','token2','...'); // up to 1000 in one request
+*/
+public function sendMessage($data,$target){
+//FCM api URL
+$url = 'https://fcm.googleapis.com/fcm/send';
+//api_key available in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+$server_key = ' AIzaSyDkV_TxAApkulxaUBgu_8FLY7H2WrFAiIo';
+			
+$fields = array();
+$fields['data'] = $data;
+if(is_array($target)){
+	$fields['registration_ids'] = $target;
+}else{
+	$fields['to'] = $target;
 }
-  }}
-
-       
- 
-										
-										
-
-
-
-?>
+//header with content_type api key
+$headers = array(
+	'Content-Type:application/json',
+  'Authorization:key='.$server_key
+);
+			
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+$result = curl_exec($ch);
+if ($result === FALSE) {
+	die('FCM Send Error: ' . curl_error($ch));
+}
+curl_close($ch);
+return $result;
+}
